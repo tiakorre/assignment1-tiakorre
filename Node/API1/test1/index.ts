28d85f166de25b74436509ff77a5e2e60b178966
tiakorre <tiakorre@example.com>
2024-02-29 20:34:10 -0500

    index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,15 +1,16 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+// const name = (req.query.name || (req.body && req.body.name));
+// const responseMessage = name
+//     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     const currentDate = new Date().toLocaleDateString();
     const responseMessage = currentDate
-        ? "Today's date is: " + currentDate
+        ? "Today's date is: " + currentDate + "."
         : "Unable to retrieve today's date.";
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
