6b548752825540e47a6a65d36d658f8ed0e2f73f
Tia Hannah <65368702+tiakorre@users.noreply.github.com>
2024-03-28 11:20:38 -0400

    Update index.js

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const { DefaultAzureCredential, SecretClient } = require("@azure/keyvault-secrets");
+
+ 
+const credential = new DefaultAzureCredential();
+const vaultUrl = "https://tiakorrekv.vault.azure.net/"; 
+const secretName = "secret1";
+
+const secretClient = new SecretClient(vaultUrl, credential);
 
 app.use('/', express.static('frontend/build'));
 
-app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+app.get('/api', async (req, res) => {
+  try {
+    const secret = await secretClient.getSecret(secretName);
+    res.send(`Hello, world! This is ${secret.value}`);
+  } catch (error) {
+    console.error("Error fetching secret:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
-  console.log('Server listening on port '+ port);
+  console.log('Server listening on port ' + port);
 });
