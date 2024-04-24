86b58655d5c010fef44327b114ee1716d0b0f065
Tia Hannah <65368702+tiakorre@users.noreply.github.com>
2024-04-24 18:11:20 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,5 +1,4 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-
 import { DefaultAzureCredential, SecretClient } from "@azure/identity";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
@@ -10,10 +9,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const secretClient = new SecretClient(vaultUrl, credential);
 
     try {
-     
         const secretName = "secret2"; // Replace with the name of your secret
         const secretValue = await secretClient.getSecret(secretName);
-        context.log('Retrieved secret value:', secretValue.value);    
+        context.log('Retrieved secret value:', secretValue.value);
         const responseMessage = `Today's date is: ${new Date().toLocaleDateString()}. Secret value retrieved from Key Vault: ${secretValue.value}`;
         context.res = {
             body: responseMessage
